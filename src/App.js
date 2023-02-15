12df16a053577108fd4451fb65aa4c14701e7cf7
Andrey <dil2232@gmail.com>
2023-02-15 19:57:37 +0600

    Add fetching tasks from JSON Placeholder API.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import InputText from "./components/Input/InputText";
-import Button from "./components/button/Button";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
+import { Oval } from "react-loader-spinner";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Something went wrong");
+      })
+      .then((data) => setTasks(data))
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   const addTask = (newTaskTitle) => {
     let newTask = { id: Date.now(), title: newTaskTitle, isCompleted: false };
@@ -31,10 +47,25 @@ function App() {
     <div className="App">
       <div className="container">
         <TaskForm addTask={addTask} />
+        {isLoading && (
+          <Oval
+            height={80}
+            width={80}
+            color="#48abe0"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel="oval-loading"
+            secondaryColor="#48abe0"
+            strokeWidth={3}
+            strokeWidthSecondary={2}
+          />
+        )}
         <TaskList
           tasks={tasks}
           deleteTask={deleteTask}
           togleTaskStatus={togleTaskStatus}
+          isLoading={isLoading}
         />
       </div>
     </div>
